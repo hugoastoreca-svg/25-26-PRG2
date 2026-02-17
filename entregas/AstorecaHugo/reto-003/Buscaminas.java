bfd4535ba8943d5cb2456d3e3dc62c622d56bda2
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-02-17 13:15:11 +0100

    Quito las arrays de los tableros y hago unmetodo estatico que cree los tableros

--- entregas/AstorecaHugo/reto-003/Buscaminas.java
+++ entregas/AstorecaHugo/reto-003/Buscaminas.java
@@ -18,26 +18,8 @@ static final int MINA = -1;
 	int posy=0;
 	boolean salida=false;
 	
-	String tablero [][] = new String [6][8];
-	
-	tablero[0][0]= "**";	tablero[0][1]= " 1";	tablero[0][2]= " 2";	tablero[0][3]= " 3";	tablero[0][4]= " 4";	tablero[0][5]= " 5";	tablero[0][6]= " 6";	tablero[0][7]= " 7";
-	tablero[1][0]= " 1";	tablero[1][1]= "  ";	tablero[1][2]= "  ";	tablero[1][3]= "  ";	tablero[1][4]= "  ";	tablero[1][5]= "  ";	tablero[1][6]= "  ";	tablero[1][7]= "  ";
-	tablero[2][0]= " 2";	tablero[2][1]= "  ";	tablero[2][2]= "  ";	tablero[2][3]= "  ";	tablero[2][4]= "  ";	tablero[2][5]= "  ";	tablero[2][6]= "  ";	tablero[2][7]= "  ";
-	tablero[3][0]= " 3";	tablero[3][1]= "  ";	tablero[3][2]= "  ";	tablero[3][3]= "  ";	tablero[3][4]= "  ";	tablero[3][5]= "  ";	tablero[3][6]= "  ";	tablero[3][7]= "  ";
-	tablero[4][0]= " 4";	tablero[4][1]= "  ";	tablero[4][2]= "  ";	tablero[4][3]= "  ";	tablero[4][4]= "  ";	tablero[4][5]= "  ";	tablero[4][6]= "  ";	tablero[4][7]= "  ";
-	tablero[5][0]= " 5";	tablero[5][1]= "  ";	tablero[5][2]= "  ";	tablero[5][3]= "  ";	tablero[5][4]= "  ";	tablero[5][5]= "  ";	tablero[5][6]= "  ";	tablero[5][7]= "  ";
-	
-	
-	
-	String tablero2 [][] = new String[6][8];
-	
-	tablero2[0][0]= "**";	tablero2[0][1]= " 1";	tablero2[0][2]= " 2";	tablero2[0][3]= " 3";	tablero2[0][4]= " 4";	tablero2[0][5]= " 5";	tablero2[0][6]= " 6";	tablero2[0][7]= " 7";
-	tablero2[1][0]= " 1";	tablero2[1][1]= "  ";	tablero2[1][2]= "  ";	tablero2[1][3]= "  ";	tablero2[1][4]= "  ";	tablero2[1][5]= "  ";	tablero2[1][6]= "  ";	tablero2[1][7]= "  ";
-	tablero2[2][0]= " 2";	tablero2[2][1]= "  ";	tablero2[2][2]= "  ";	tablero2[2][3]= "  ";	tablero2[2][4]= "  ";	tablero2[2][5]= "  ";	tablero2[2][6]= "  ";	tablero2[2][7]= "  ";
-	tablero2[3][0]= " 3";	tablero2[3][1]= "  ";	tablero2[3][2]= "  ";	tablero2[3][3]= "  ";	tablero2[3][4]= "  ";	tablero2[3][5]= "  ";	tablero2[3][6]= "  ";	tablero2[3][7]= "  ";
-	tablero2[4][0]= " 4";	tablero2[4][1]= "  ";	tablero2[4][2]= "  ";	tablero2[4][3]= "  ";	tablero2[4][4]= "  ";	tablero2[4][5]= "  ";	tablero2[4][6]= "  ";	tablero2[4][7]= "  ";	
-	tablero2[5][0]= " 5";	tablero2[5][1]= "  ";	tablero2[5][2]= "  ";	tablero2[5][3]= "  ";	tablero2[5][4]= "  ";	tablero2[5][5]= "  ";	tablero2[5][6]= "  ";	tablero2[5][7]= "  ";			
-		
+	String tablero [][]= crearTablero();
+    String tablero2 [][]= crearTablero();
 	
 	
 		posa=(int)((Math.random()*FILAS)+1);
@@ -143,4 +125,23 @@ static final int MINA = -1;
 	}
 		
 	}
+
+static String[][] crearTablero(){
+    String tablero [][]= new String [FILAS +1][COLUMNAS +1];
+    tablero[0][0]= "**";
+    for (int i=1 ;i<=FILAS; i++){
+        tablero[i][0]=""+i;
+
+    }
+    for (int j=1; j<=COLUMNAS; j++){
+        tablero[0][j]=""+j;
+        for( int i = 1 ; i <= FILAS; i++){
+            tablero[i][j]=" ";
+        }
+
+    }
+return tablero;
+}
+
+
 }
