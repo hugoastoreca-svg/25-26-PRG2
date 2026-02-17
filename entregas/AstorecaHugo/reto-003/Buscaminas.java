b9a74a6a6dc53f24a7954f0c5f66dc4282f8baa6
hugoastoreca <hugo.astoreca@alumnos.uneatlantico.es>
2026-02-17 13:48:45 +0100

    Creo el metodo estatico de si es mina o no

--- entregas/AstorecaHugo/reto-003/Buscaminas.java
+++ entregas/AstorecaHugo/reto-003/Buscaminas.java
@@ -22,8 +22,8 @@ static final int MINA = -1;
     String tablero2 [][]= crearTablero();
 	
 	colocarMinas(tablero);
-		
-		
+	pedirCoordenadas(posx,posy);	
+	esMina(tablero, posx, posy);	
 		
 		
 	while(!salida){
@@ -37,39 +37,12 @@ static final int MINA = -1;
 	
 	
 	
-	if ((tablero[posx][posy])=="  "){
-		tablero2[posx][posy]= "--";
-		celdasVacias++;
-	}
-	else{
-		tablero2[posx][posy]= "**";
-		minas++;
-	}
 	
-	
-	for (int x=0; x < tablero2.length; x++) {
-		System.out.print("|");
-		for (int y=0; y < 8; y++){
-	
-			System.out.print (tablero2[x][y]);
-		}
-		System.out.println("|");
-	}
-
-		
-		if (minas==3){
-		System.out.println("Lo siento, ha perdido");
-		salida=true;
-		}
-		
-		if (celdasVacias==30){
-			System.out.println("Enhorabuena, ha ganado");
-			salida=true;
 		}
 	
 	}
 		
-	}
+	
 
 static String[][] crearTablero(){
     String tablero [][]= new String [FILAS +1][COLUMNAS +1];
@@ -87,6 +60,7 @@ static String[][] crearTablero(){
     }
 return tablero;
 }
+
 static void mostrarTablero(String tablero[][]){
     for (int x=0 ; x< tablero.length; x++){
         System.out.print("|");
@@ -108,6 +82,7 @@ static void colocarMinas(String tablero[][]){
         }
     }
 }
+
 static void pedirCoordenadas(int posx, int posy){
     Scanner scanner = new Scanner(System.in);
     System.out.println("Introduce la coordenada x:");
@@ -115,3 +90,14 @@ static void pedirCoordenadas(int posx, int posy){
     System.out.println("Introduce la coordenada y:");
     posy = scanner.nextInt();
 }
+
+static boolean esMina(String tablero[][], int posx, int posy){
+    return tablero[posx][posy]=="*";
+
+}
+
+
+
+}
+
+
